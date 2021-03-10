45b646bef0142ddcf4e5234f9a4edb5016819131
D Marcus <mindlesscorporatedrone@gmail.com>
2021-03-09 18:38:18 -0800

    styling added

--- src/Joke.js
+++ src/Joke.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import './Joke.css';
 
 class Joke extends Component {
     constructor(props) {
         super(props)
         this.handleAdd = this.handleAdd.bind(this);
         this.handleDecrease = this.handleDecrease.bind(this);
+    }
+
+    getColor() {
+        if (this.props.score >= 15) {
+            return "#4CAF50";
+        } else if (this.props.score >= 12) {
+            return "#8BC34A";
+        } else if (this.props.score >= 9) {
+            return "#CDDC39";
+        } else if (this.props.score >= 6) {
+            return "#FFEB3B";
+        } else if (this.props.score >= 3) {
+            return "#FFC107";
+        } else if (this.props.score >= 0) {
+            return "#FF9800";
+        } else {
+            return "#f44336";
+        }
+    }
+    getEmoji() {
+        if (this.props.score >= 15) {
+            return "em em-rolling_on_the_floor_laughing";
+        } else if (this.props.score >= 12) {
+            return "em em-laughing";
+        } else if (this.props.score >= 9) {
+            return "em em-smiley";
+        } else if (this.props.score >= 6) {
+            return "em em-slightly_smiling_face";
+        } else if (this.props.score >= 3) {
+            return "em em-neutral_face";
+        } else if (this.props.score >= 0) {
+            return "em em-confused";
+        } else {
+            return "em em-angry";
+        }
     }
 
     handleAdd() {
@@ -18,13 +54,19 @@ class Joke extends Component {
 
     render() {
         return (
-            <div>
-                <p>{this.props.text}</p>
-                <button onClick={this.handleAdd}>Add</button>
-                <p>{this.props.score}</p>
-                <button onClick={this.handleDecrease}>Decrease</button>
-                <hr/>
-            </div>
+            <div className="Joke">
+                <div className="Joke-buttons">
+                    <i className='fas fa-arrow-up' onClick={this.handleAdd} />
+                    <span className='Joke-votes' style={{ borderColor: this.getColor() }}>
+                        {this.props.score}
+                    </span>
+                    <i className='fas fa-arrow-down' onClick={this.handleDecrease} />
+                </div>
+                <div className='Joke-text'>{this.props.text}</div>
+                <div className='Joke-smiley'>
+                    <i className={this.getEmoji()} />
+                </div>
+            </div >
         )
     }
 }
--- src/JokeList.js
+++ src/JokeList.js
@@ -70,9 +70,20 @@ class JokeList extends Component {
         ))
         return (
             <div className="JokeList">
-                <h1>Dad Jokes</h1>
-                <button onClick={this.handleClick}>New Jokes</button>
-                <div className="JokeList-jokes">{allJokes}</div>
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title">
+                        <span>Dad</span> Jokes
+                    </h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                    <button className='JokeList-getmore' onClick={this.handleClick}>
+                        Fetch Jokes
+                    </button>
+                </div>
+
+                <div className="JokeList-jokes">
+                {allJokes}
+                </div>
+                
             </div>
         )
     }
